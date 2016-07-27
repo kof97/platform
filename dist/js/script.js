e4a752a13ec56c888f2fc047274e9368b312fc4e
kof97 <1048434786@qq.com>
2016-07-27 17:05:06 +0800

    changed

--- dist/js/script.js
+++ dist/js/script.js
@@ -11810,7 +11810,7 @@ var func = {
 			return 0;
 		}
 
-		var flag = "/ads/v3/",
+		var flag = "/ads/" + selector.versions.val() + "/",
 			flagLength = flag.length,
 			flagStartIndex, flagEndIndex, divideIndex, pathLength, hostname, path, mod, act, param;
 
