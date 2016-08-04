639c9574efc5879c3cbe1dec4ed0e3ccf81ad6ac
kof97 <1048434786@qq.com>
2016-08-04 20:12:44 +0800

    sdk

--- html/doc.php
+++ html/doc.php
@@ -1,11 +1,21 @@
 <?php include './layout/header.php'; ?>
 
-	<div class="header" id="canvas_wrap" style="display: inline-block;">
-		<canvas id="canvas"></canvas>
+	<div class="container">
+		<div class="doc-list">
+			<a href="docs/App.md.html" target="docs">qweqwe</a>
+			<a href="docs/App.md.html" target="docs"></a>
+			<a href="docs/App.md.html" target="docs"></a>
+			<a href="docs/App.md.html" target="docs"></a>
+			<a href="docs/App.md.html" target="docs"></a>
+		</div>
+		<iframe name="docs" id="docs" src="docs/Spa.md.html" onload="console.log(docs.document.body.scrollHeight);"></iframe>
+
 	</div>
+	<script>
+		
+		var doc = document.getElementById('docs');
+		doc.height = docs.document.body.scrollHeight;
 
 
-
-
-
+	</script>
 <?php include './layout/footer.php'; ?>
