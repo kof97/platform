e4665bbba761f233dd1e2d7e470b66486f7ab9a1
kof97 <1048434786@qq.com>
2016-08-04 19:51:23 +0800

    sdk

---
+++ html/doc.php
@@ -0,0 +1,11 @@
+<?php include './layout/header.php'; ?>
+
+	<div class="header" id="canvas_wrap" style="display: inline-block;">
+		<canvas id="canvas"></canvas>
+	</div>
+
+
+
+
+
+<?php include './layout/footer.php'; ?>
