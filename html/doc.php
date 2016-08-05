f528fcd62b360ac26170c2439b207eb14c30d21e
kof97 <1048434786@qq.com>
2016-08-05 10:38:58 +0800

    UI优化

--- html/doc.php
+++ html/doc.php
@@ -8,13 +8,13 @@
 			<a href="docs/Request.md.html" target="docs">\Request</a>
 			<a href="docs/Client.md.html" target="docs">\Client</a>
 			<a href="docs/Response.md.html" target="docs">\Response</a>
-
+			<hr>
 			<span><strong>Spa\Authentication\</strong></span>
 			<a href="docs/AccessToken.md.html" target="docs">\AccessToken</a>
-
+			<hr>
 			<span><strong>Spa\Object\Interfaces\</strong></span>
 			<a href="docs/Interfaces.md.html" target="docs">\{接口发送实体类}</a>
-
+			<hr>
 			<span><strong>Spa\Object\Enum\</strong></span>
 			<a href="docs/Enum.md.html" target="docs">\{接口参数枚举类}</a>
 		</div>
