ded03b47e249ac9a31c0cae74484b8de8fdac5cf
kof97 <1048434786@qq.com>
2016-08-05 09:30:51 +0800

    UI优化

--- html/layout/header.php
+++ html/layout/header.php
@@ -19,12 +19,12 @@
 			<a href="./index.php" class="logo">
 				<strong>SPA</strong> for developers |
 			</a>
+			<a href="./index.php">首页</a>
 			<a href="">OPEN SDK</a>
 			<a href="./doc.php">文档</a>
 			<a href="./tools.php" class="">工具</a>
 			<a href="#">新闻</a>
 			<a href="#">支持中心</a>
 			<a href="#">社区</a>
-
 		</header>
 	</nav>
